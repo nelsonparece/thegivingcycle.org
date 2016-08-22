de1ebb7ad596484de0e2883140fbe708bd7f8eeb
thegivingcycle <jesse.josserand@gmail.com>
2016-08-21 23:54:44 -0400

    yet another try

---
+++ contact/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML>
+<HTML LANG="en">
+<HEAD>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<meta name="generator" content="Bootply" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Contact Us | The Giving Cycle | www.TheGivingCycle.org | Victory in Giving LLC</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<meta name="keywords" content="">
+<meta name="description" content="Contact The Giving Cycle and Victory in Giving LLC at 1-855-397-3339">
+
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<link href="css/styles.css" rel="stylesheet">
+
+<STYLE>
+body, div, p {
+        font-family: times new roman, times, georgia, serif;
+}
+</STYLE>
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<?php include("js/ga.js"); ?>
+
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"><img src="images/brand.png"></a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="../index.php" style="color: white;">Home</a></li>
+				<li><a target="_blank" href="../index.live.php">More Info</a></li>
+				<li><a target="_blank" href="../aboutus.php">About Us</a></li>
+				<li><a target="_blank" href="contact">Contact Us</a></li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div>
+</div>
+
+<!-- 4 stripes for branding -->
+<div class="text-center" style="height: 4rem; width: 100%; color: white; vertical-align: middle; text-align: center; font-size: 3rem; background-color: #ecb212;">Interests And Education</div><!-- gold -->
+<div class="text-center" style="height: 4rem; width: 100%; color: white; vertical-align: middle; text-align: center; font-size: 3rem; background-color: #6cacde;">Philanthropy</div><!-- blue -->
+<div class="text-center" style="height: 4rem; width: 100%; color: white; vertical-align: middle; text-align: center; font-size: 3rem; background-color: #149a77;">Discount Shopping</div><!-- green -->
+<div class="text-center" style="height: 4rem; width: 100%; color: white; vertical-align: middle; text-align: center; font-size: 3rem; background-color: #98012e;">Social Interaction</div><!-- red -->
+<!-- end 4 stripes for branding -->
+
+<div class="container">
+	<div class="row">
+		<div class="text-center">
+			<h1>Contact The Giving Cycle&trade;</h1>
+		</div>
+	</div>
+</div>
+
+<div style="margin-top: 3rem; margin-bottom: 3rem; !important;"></div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-12">
+			<div class="contact-form">
+        			<h1>Contact Us</h1>
+				<form id="contact-form" method="POST" action="form-to-email.php">
+				<div class="control-group">
+					<label>Your Name</label>
+					<input class="fullname" type="text" name="fullname" />
+				</div>
+				<div class="control-group">
+					<label>Email</label>
+					<input class="email" type="text" name="email" />
+				</div>
+				<div class="control-group">
+					<label>Phone (optional)</label>
+					<input class="phone" type="text" name="phone" />
+				</div>
+				<div class="control-group">
+					<label>Message</label>
+					<textarea class="message" name="message"></textarea>
+				</div>
+				<div id="errors"></div>
+					<div class="control-group no-margin">
+						<input type="submit" name="submit" value="Submit" id="submit" />
+					</div>
+				</form>
+        			<div id='msg_submitting'><h2>Submitting ...</h2></div>
+        			<div id='msg_submitted'><h2>Thank you !<br> The form was submitted Successfully.</h2></div>
+			</div>
+
+		</div>
+	</div><!-- /.row -->
+</div><!-- /.container -->
+
+<div style="margin: 0 0 5rem 0 !important;"></div>
+
+<div class="container">
+	<div class="row">
+		<div class="text-center">
+			<h1>Join Us And Contribute Now!</h1>
+			<?php include("inc/contribute.inc"); ?>
+			<p class="lead">We know You are generous or You wouldn't be here. Nevertheless, You may contribute any amount You choose. We are trying to raise a total of $90,000.00. A $99 contribution makes You an automatic full founding member, and locks in Your annual membership price at $99 per year for life. By making such a contribution during our pre-launch, Your membership is automatically paid up through December 31, 2017! Founding members receive other special benefits, as well.</p>
+			<p class="lead">If you would like more information about the benefits of founding membership, <a href="../aboutus.php" target="_blank">click here to learn more</a>.</p>
+		</div>
+	</div><!-- /.row -->
+</div><!-- /.container -->
+
+<div style="margin: 0 0 5rem 0 !important;"></div>
+
+<!-- div id="footer" class="navbar-fixed-bottom" style="background-color:#333333;" -->
+<div id="footer" class="navbar-fixed-bottom" style="background-color:#333333;">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<a href="../index.php">&copy; 2016 The Giving Cycle&trade;</a><br>
+				<a target="_blank" href="../privacy.php">Privacy</a> | <a target="_blank" href="../terms.php">Terms</a> | <a target="_blank" href="../refunds.php">Refunds</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- script references -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- JavaScript Files -->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="js/additional-methods.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+<!-- End JavaScript Files -->
+
+<script>
+  (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+    t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+  })(window, document, '_gscq','script','//widgets.getsitecontrol.com/49909/script.js');
+</script>
+
+</body>
+</html>
